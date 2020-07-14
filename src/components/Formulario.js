a8a00227f53c36c2aaa17ae429335b4b836516d6
Fern4ndox <fern4ndorecinos@gmail.com>
2020-07-13 19:03:51 -0600

    Primer Formulario Terminado

---
+++ src/components/Formulario.js
@@ -0,0 +1,49 @@
+import React,{Fragment} from 'react';
+
+const Formulario = () => {
+    return ( 
+        <Fragment>
+            <h2>Crear Cita</h2>
+            <form>
+                <label>Nombre Mascota</label>
+                <input
+                type="text"
+                name="mascota"
+                className="u-full-width"
+                placeholder="Nombre Mascota"
+                />
+                <label>Nombre Dueño</label>
+                <input
+                type="text"
+                name="propietario"
+                className="u-full-width"
+                placeholder="Nombre Dueño de la Mascota"
+                />
+                <label>Fecha</label>
+                <input
+                type="date"
+                name="fecha"
+                className="u-full-width"
+                />
+                <label>Hora</label>
+                <input
+                type="time"
+                name="hora"
+                className="u-full-width"
+                />
+                <label>Sintomas</label>
+               <textarea
+               className="u-full-width"
+               name="sintomas"
+               />
+                <button
+                type="submit"
+                className="u-full-width button-primary"
+                >Agregar Cita</button>
+            </form>
+        </Fragment>
+     )
+    
+}
+ 
+export default Formulario;
