ecaac5b24a5b2ef4ddc9366b92df7051fda68675
Fern4ndox <fern4ndorecinos@gmail.com>
2020-07-15 15:17:23 -0600

    Proyecto Finalizado

--- src/components/Formulario.js
+++ src/components/Formulario.js
@@ -1,5 +1,6 @@
 import React,{Fragment, useState} from 'react';
 import { v4 as uuidv4 } from 'uuid';
+import PropTypes from 'prop-types';
 
 const Formulario = ({crearCita}) => {
     //Crear State de Citas
@@ -109,6 +110,10 @@ const Formulario = ({crearCita}) => {
         </Fragment>
      )
     
+}
+
+Formulario.propTypes = {
+    crearCita: PropTypes.func.isRequired
 }
  
 export default Formulario;
